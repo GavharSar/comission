67985e07bbf14e3efeafaeca03a08051aa666fc8
Gavkhar Saryeva <eibova94@mail.ru>
2023-02-15 11:57:05 +0300

    edit code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,24 @@
+import kotlin.math.max
+
+const val TYPE_VISA = "VISA"
+const val TYPE_MIR = "Мир"
+const val TYPE_MASTERCARD = "MasterCard"
+const val TYPE_MAESTRO = "Maestro"
+const val TYPE_VK = "VK Pay"
+const val ERROR_TYPE = -1
+const val ERROR_LIMIT = -2
+
+
 fun main() {
-    val amound = 100_000
-    val paymentSystem = "VK Pay"
-    var previousTranslationsVK = 0
-    var previousTranslations = 0
-    val minComission = 35
-    val maxComission = 0.0075
-    val limit = 75_000
-    val mComission = 0.006
 
-    var commission1 = commission(paymentSystem, amound, maxComission, minComission, limit, mComission)
+    val comission = comission(TYPE_VK, 100_000, 0)
 
-    println("Сумма комиссии состаяляет $commission1 рублей")
-    println()
-    if (paymentSystem == "VK Pay") limitsVK(previousTranslationsVK, amound) else limits(previousTranslations, amound)
+    println(comission)
 }
 
-fun commission(paymentSystem: String, amound: Int, maxComission: Double, minComission: Int, limit: Int, mComission: Double) = when (paymentSystem) {
-    "Visa", "Мир" -> if (amound * maxComission < minComission) minComission else amound * maxComission
-    "MasterCard", "Maestro" -> if (amound < limit) 0 else amound * mComission + 20
-    else -> 0
-}
-
-fun limitsVK(previousTranslationsVK: Int, amound: Int) {
-        if (amound > 15_000 || previousTranslationsVK > 40_000 || previousTranslationsVK + amound > 40_000) println("Превышена сумма лимита перевода")
-        else println("Перевод успешно выполнен"); previousTranslationsVK + amound
-}
-
-fun limits(previousTranslations: Int, amound: Int) {
-    if (amound > 150_000 || previousTranslations > 600_000 || previousTranslations + amound > 600_000) println("Превышена сумма лимита перевода")
-    else println("Перевод успешно выполнен"); previousTranslations + amound
+fun comission(paymentSystem: String, amound: Int, previousTranslations: Int) = when (paymentSystem) {
+    TYPE_VISA, TYPE_MIR -> if (amound < 150_000 && amound + previousTranslations <= 600_000) max(35, (amound * 0.0075f).toInt()) else ERROR_LIMIT
+    TYPE_MAESTRO, TYPE_MASTERCARD -> if (amound < 150_000 && amound + previousTranslations <= 600_000 && amound <= 75_000) 0 else if (amound < 150_000 && amound + previousTranslations <= 600_000) (amound * 0.006f).toInt() + 20 else ERROR_LIMIT
+    TYPE_VK -> if (amound < 15_000 && amound + previousTranslations <= 40_000) 0 else ERROR_LIMIT
+    else -> ERROR_TYPE
 }
